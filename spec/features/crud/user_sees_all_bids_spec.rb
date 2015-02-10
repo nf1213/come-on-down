2e5f986430663734b8d1aff794ad27ef8c9bc638
Zoe <doticus@gmail.com>
2015-02-09 21:32:55 -0500

    Index is working

--- spec/features/crud/user_sees_all_bids_spec.rb
+++ spec/features/crud/user_sees_all_bids_spec.rb
@@ -12,7 +12,7 @@ So that I don't go out of my everloving mind from cabin fever
 # [x] I can see who won the prizes that are closed
 # [x] I can see the prize price of the prizes that are closed
 
-  scenario 'User sees all of the prizes', focus: true do
+  scenario 'User sees all of the prizes' do
 
     user = FactoryGirl.create(:user)
     prize = FactoryGirl.create(:prize)
