4d87b555fd840dca5d3e08350bd5a902b636eea4
Nicole <nfelch121394@gmail.com>
2015-02-09 20:43:04 -0500

    Admin prize controller and find_winner function added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authorize!
+    if current_user.nil? or !current_user.admin
+      redirect_to prizes_path, notice: "You are not authorized to view this resource."
+    end
+  end
 end
